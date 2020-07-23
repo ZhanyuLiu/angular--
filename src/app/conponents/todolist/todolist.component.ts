35ecc4f9692b7dafedfe243a9a3474cbb391f236
ZhanyuLiu <1635259245@qq.com>
2020-07-23 19:34:42 +0800

    rxjs异步数据

--- src/app/conponents/todolist/todolist.component.ts
+++ src/app/conponents/todolist/todolist.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { StorageService } from "../../service/storage.service";
 
+import { map, filter } from "rxjs/operators";
+import { pipe } from 'rxjs';
+
 /*服务的使用：
 1、创建服务：ng g service service/storage
 2、app.module.ts里面引入创建的服务，并且声明
@@ -32,10 +35,80 @@ export class TodolistComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // 1.同步方法
     var todoList = this.storage.get('todolist');
     if(todoList) {
       this.todolist = todoList;
     }
+
+    // 2.callback获取异步数据
+    this.storage.getCallbackData((data)=>{
+      console.log(data);
+    });
+
+    // 3.Promise获取异步数据
+    var promiseData = this.storage.getPromiseData();
+    promiseData.then((data)=> {
+      console.log(data);
+    });
+
+    // // 4.rxjs获取异步方法里面的数据
+    // var rxjsData = this.storage.getRxjsData();
+    // rxjsData.subscribe((data)=> {
+    //   console.log(data);
+    // })
+
+    // 5.过一秒以后撤回刚才的操作
+    var streen = this.storage.getRxjsData();
+     var d = streen.subscribe((data)=> {
+      console.log(data);
+    });
+
+    setTimeout(()=> {
+      d.unsubscribe();  // 取消订阅
+    },1000);
+
+    // 6.promise 执行多次（没有这个能力）
+    var IntervalData = this.storage.getPromiseIntervalData();
+    IntervalData.then((data)=> {
+      console.log(data);
+    });
+
+    // 7.rxjs执行多洗 
+    // var streenInterval = this.storage.getRxjsIntervalData();
+    //  streenInterval.subscribe((data)=> {
+    //   console.log(data);
+    // });
+
+    // 8.用工具方法对返回的数据进行处理
+    // filter
+    // var streenNum = this.storage.getRxjsIntervalNum();
+    // streenNum.pipe(
+    //   filter((value) => {
+    //     if(value % 2 == 0){
+    //       return true;
+    //     }
+    //   })
+    // ).subscribe((data)=> {
+    //   console.log(data);
+    // });
+
+
+    // map
+    var streenNum = this.storage.getRxjsIntervalNum();
+    streenNum.pipe(
+      filter((value) => {
+            if(value % 2 == 0){
+              return true;
+            }
+          }),
+      map((value) => {
+        return value * value
+      })
+    ).subscribe((data)=> {
+      console.log(data);
+    });
+
   }
 
   doAdd(e) {
--- src/app/service/storage.service.ts
+++ src/app/service/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class StorageService {
   set(key:string, value:any) {
     localStorage.setItem(key, JSON.stringify(value));
   }
-
+ // 同步方法
   get(key:string) {
     // return 'this is a service';
     return JSON.parse(localStorage.getItem(key));
@@ -18,5 +19,65 @@ export class StorageService {
 
   remove(key:string) {
     localStorage.removeItem(key);
+  }
+
+
+  // 异步方法
+
+  getCallbackData(cb) {
+    setTimeout(() => {
+      var data = "张三";
+      // return data;
+      cb(data);
+    },1000);
+  }
+
+  getPromiseData() {
+    return new Promise((resolve, reject)=> {
+      setTimeout(() => {
+        var data = "张三--promise";
+        resolve(data)
+      },3000);
+    });
+  }
+
+  getRxjsData() {
+    return new Observable((observe)=> {
+      setTimeout(() => {
+        var data = "张三--observe";
+        observe.next(data)
+      },3000);
+    });
+  }
+
+// 多次执行
+  getPromiseIntervalData() {
+    return new Promise((resolve, reject)=> {
+      setInterval(() => {
+        var data = "张三--Interval";
+        resolve(data)
+      },3000);
+    });
+  }
+
+  getRxjsIntervalData() {
+    let count = 0;
+    return new Observable((observe)=> {
+      setInterval(() => {
+        count++;
+        var data = "张三--RxjsInterval" + count;
+        observe.next(data)
+      },2000);
+    });
+  }
+
+  getRxjsIntervalNum() {
+    let count = 0;
+    return new Observable<any>((observe)=> {
+      setInterval(() => {
+        count++;
+        observe.next(count)
+      },1000);
+    });
   }
 }
